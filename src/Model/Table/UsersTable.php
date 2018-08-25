e338c04c7a1e62beb0ea93ff0bde33688d4a8f78
Bahlul <bahlul.siddiquee@sjinnovation.com>
2018-08-25 20:51:39 +0600

    Added the registration system with password validation and confirm password confirmation validation system

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -65,17 +65,59 @@ class UsersTable extends Table
             ->requirePresence('email', 'create')
             ->notEmpty('email');
 
+/* Create a new custom password validor for cakephp in blow this build in method
+
         $validator
             ->scalar('password')
             ->maxLength('password', 50)
             ->requirePresence('password', 'create')
             ->notEmpty('password');
+*/
 
+        //Custom password validator method
+
+         $validator
+         ->add(
+            'password',
+            [
+                'minLength' => 
+                [
+                    'rule' => ['minLength',6],
+                    'message' => "The password length minimum 6 charecter"
+                ],
+            ]
+
+        )
+           ->requirePresence('password','create','Password Must Be Required')
+            ->notEmpty('password','Password must be required');
+
+
+        //Custom password confimation validator method
+        $validator
+            ->requirePresence('confirm_password', 'create', 'Password must be required!')
+            ->notEmpty('confirm_password', 'Confirm password must be required!')
+            ->add(
+                'confirm_password',
+                'custom',
+                [
+                    'rule' => function ($value, $context) {
+                            if (isset($context['data']['password']) && $value == $context['data']['password']) {
+                                return true;
+                            }
+                            return false;
+                        },
+                    'message' => 'Sorry, password and confirm password does not matched'
+                ]
+            );
+         
+
+/*
         $validator
             ->scalar('confirm_password')
             ->maxLength('confirm_password', 50)
             ->requirePresence('confirm_password', 'create')
             ->notEmpty('confirm_password');
+*/
 
         return $validator;
     }
